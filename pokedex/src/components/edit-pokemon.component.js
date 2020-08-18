2808b458fca112f9024ba6ece1025e326686f2d2
bruno pessoa <brunopessoa.melo@gmail.com>
2020-08-18 18:32:35 -0300

    criando os componentes e instalando o react-router-dom

---
+++ pokedex/src/components/edit-pokemon.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class EditPokemon extends Component{
+    render(){
+        return(
+            <div>
+                <p>React Edit Pokemon Component</p>
+            </div>
+        )
+    }
+}
