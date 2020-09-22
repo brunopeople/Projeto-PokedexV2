6f034b826d41ba82d4a3d3e3d88391925f95abc9
bruno pessoa <brunopessoa.melo@gmail.com>
2020-09-22 16:29:27 -0300

    alterando as rotas com o modulo consign.

--- PokeAPIRESTFULL/routes/pokemons.js
+++ PokeAPIRESTFULL/routes/pokemons.js
@@ -1,17 +1,23 @@
-let express = require('express');
-let routes = express.Router();
 
-routes.get('/pokemons', (req,res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'application/json');
-    res.json({
-        pokemons: [{
-            name: 'Pikachu',
-            type: 'Eletric',
-            color:'yellow',
-            id: 1
-        }]
+module.exports = function(app){
+    app.get('/pokemons', (req,res) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json({
+            pokemons: [{
+                name: 'Pikachu',
+                type: 'Eletric',
+                color:'yellow',
+                id: 1
+            }]
+        });
     });
-});
 
-module.exports = routes;
+    app.get('/admin', (res, req) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json({
+            pokemons:[]
+        });
+    });
+};
