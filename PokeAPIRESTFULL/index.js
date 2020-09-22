0b3d10083625971031b79b3cb53a1dded57f3042
bruno pessoa <brunopessoa.melo@gmail.com>
2020-09-22 16:16:53 -0300

    separando as rotas em arquivos diferentes no arquivo principal.

--- PokeAPIRESTFULL/index.js
+++ PokeAPIRESTFULL/index.js
@@ -1,37 +1,12 @@
-const http = require('http'); //chamando a o módulo http 
+const express = require('express');
+let routesIndex = require('./routes/index');
+let routesPokemons = require('./routes/pokemons');
 
+let app = express();
 
-// criando o servidor local, definindo a variável server, instanciando o modulo http e chamando o método
-// createServer, com isto recebe dois paramêtros res e req, estes parâmetros servem para quando o servidor tiver uma requisição ele responder e trazer uma 
-let server = http.createServer((req, res) => {
-    console.log('URL', req.url);
-    console.log('METHOD:', req.method);
+app.use(routesIndex);
+app.use(routesPokemons);
 
-    switch(req.url){
-        case '/':
-
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/html');
-        res.end('<h1>Olá</h1>');
-
-        break;
-
-        case '/pokemons':
-
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'application/json');
-        res.end(JSON.stringify({
-            pokemons:[{
-                name: 'Pikachu',
-                type: 'Eletric',
-                color: 'yellow',
-                id: 1
-            }]
-        }));
-
-        break;
-    }
-});
-    server.listen(3000, '127.0.0.1', ()=>{
-        console.log('servidor rodando!!');
-    });
+app.listen(3000, '127.0.0.1', ()=> {
+    console.log('servidor rodando!');
+}); 
